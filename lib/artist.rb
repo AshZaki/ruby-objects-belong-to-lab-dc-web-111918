8b22693552619a1bf3a94bdd38a93b8671823831
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-26 17:56:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,32 @@
-class Artist
-  attr_accessor :name
+#class Artist
+  #attr_accessor :name
   
 
+#end
+
+require 'pry'
+class FunnyBots  
+
+   attr_accessor :name, :quotes  
+
+   @@bots = []
+
+   def initialize(name, quotes)
+     @name = name
+     @quotes = quotes
+     @@bots << self
+     binding.pry
+   end
+
+  def random_quote
+    binding.pry
+     self.quotes.sample
+   end
+
+   def self.bots
+     @@bots
+   end
+
 end
+
+ archer = FunnyBots.new("Archer", ["Danger Zone!", "Read a book", "The cumulative hangover would literally kill me"] )
